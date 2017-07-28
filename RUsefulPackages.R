473239a3dd5cd5d27a0375554d68063aa1db5581
mayankaggarwal <mayankaggarwal1989@gmail.com>
2017-07-28 21:50:50 +0530

    Adding R packages list

---
+++ RUsefulPackages.R
@@ -0,0 +1,11 @@
+install.packages('data.table')
+library(data.table)
+library(dplyr)
+library(ggplot2)
+library(knitr)
+library(stringr)
+library(DT)
+
+install.packages("installr"); library(installr)
+updateR()
+
