20bf72f7133971bc7bb5a7b041d52756afdae7fd
mayankaggarwal <mayankaggarwal1989@gmail.com>
2017-10-30 10:16:10 +0530

    Adding useful commands

--- RUsefulPackages.R
+++ RUsefulPackages.R
@@ -1,11 +1,12 @@
-install.packages('data.table')
 library(data.table)
 library(dplyr)
 library(ggplot2)
 library(knitr)
 library(stringr)
 library(DT)
-
-install.packages("installr"); library(installr)
+library(installr)
+library(xgboost)
+library(Ckmeans.1d.dp)
+install.packages("Ckmeans.1d.dp"); 
 updateR()
 
